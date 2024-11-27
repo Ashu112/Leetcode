9edaf2c4ff4beb42e03dc0842c95aabe671dfd5a
Ashutosh Kumar <kashu2611@gmail.com>
2024-11-27 16:04:11 +0530

    Time: 2 ms (84.33%) | Memory: 46 MB (35.04%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,8 +1,9 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // 1. take two pointers i,j both placed at first index
-        // 2. move j such that we swap the non zero vlaue
-        int i=0,j = 0;
+        // 1. take 2 pointers i,j and place them at start of array
+        // 2. traverse array with j pointer 
+        //      a. if j is non zero element swap with ith element
+        int i = 0 , j = 0;
         int n = nums.length;
         while(j < n){
             if(nums[j] != 0){
